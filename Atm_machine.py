32d1e94602588961e0bfd4053d6515ff37dc1fa6
sammy1209 <54069998+sammy1209@users.noreply.github.com>
2020-04-05 19:01:07 +0530

    whole_code

--- Atm_machine.py
+++ Atm_machine.py
@@ -2,7 +2,7 @@ import time
 import pyttsx3
 import datetime
 import os
-import sys
+from getpass import getpass
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -17,13 +17,13 @@ def speak(audio):
 def greetMe():
     currentH = int(datetime.datetime.now().hour)
     if currentH >= 0 and currentH < 12:
-        speak("Good Morning!, Ma'am")
+        speak("Good Morning Ma'am")
         speak("Welcome to Global Bank Services")
         time.sleep(1)
         speak("swipe your card here")
 
     if currentH >= 12 and currentH < 18:
-        speak("Good Afternoon!, Ma'am")
+        speak("Good Afternoon Ma'am")
         speak("Welcome to Global Bank Services")
         speak("swipe your card here")
 
@@ -34,46 +34,23 @@ def greetMe():
         speak("swipe your card here")
 
 greetMe()
-    
-atm_pin = 9999
 
-transation = ["Balance Enquiry", "Withdraw Money","Deposit Money", "Change your pin", "Transfer Money", "Quit"]
-
-pin = int(input(speak("Enter your 4 digit pin: ")))
-
-if pin==atm_pin:
-    
-    speak(" Choose your Transaction: ")
-    
-    print("1. Balance Enquiry")
-    print("2. Withdraw Money")
-    print("3. Deposit Money")
-    print("4. Change your pin")
-    print("5. Transfer Money")
-    print("6. Quit")
-    trans=input("transation: ")
+def Balance_Enquiry():
+    slip = input(speak("Do you want slip? "))
     time.sleep(2)
-    
-    
-    
-    if trans == "Balance Enquiry":
-        slip = input(speak("Do you want slip? "))
-        time.sleep(2)
-        speak("please wait, we are processing")
-        time.sleep(2)
-        if slip == "yes":
-            speak("Here is your slip.... ")
-            time.sleep(1)
-            speak(".....Collect your Card.....")
-            speak("Thankyou for using Global Bank Services ")
-            speak("Have a nice day, Bye...")
-        else:
-            speak("Thankyou for using Global Bank Services ")
-            speak("Have a nice day, Bye...")
-            
-            
-            
-    elif trans == "Withdraw Money":
+    if slip == "yes":
+        speak("Here is your slip.... ")
+        time.sleep(1)
+        speak(".....Collect your Card.....")
+        speak("Thankyou for using Global Bank Services ")
+        speak("Have a nice day, Bye...")
+    else:
+        speak("Thankyou for using Global Bank Services ")
+        speak("Have a nice day, Bye...")
+
+def Withdraw_Money():
+    x = int(getpass(speak("please re-enter your pin to proceed:")))
+    if x==atm_pin:
         amount = int(input(speak("Enter the amount: ")))
         if (amount>=100):
             speak("........please wait, your Transaction is under process.......")
@@ -86,11 +63,11 @@ if pin==atm_pin:
             speak("Have a nice day, Bye...")
             
         else:
-            speak("Enter valid amount")
-              
-                
-                
-    elif trans == "Deposit Money" :
+            speak("Enter valid amount") 
+        
+def Deposite_Money():
+    x = int(getpass(speak("please re-enter your pin to proceed:")))
+    if x==atm_pin:
         amount = int(input(speak("Enter a amount to deposite:")))
         time.sleep(2)
         if (amount>0):
@@ -99,42 +76,84 @@ if pin==atm_pin:
             
         else:
             speak("Enter valid amount")
-        
-        
-        
-    elif trans == "Change your pin" :
-        change_pin= int(input(speak(" Enter your new pin: ")))
-        if change_pin >=0:
-            speak("Your pin is successfully changed...., Thankyou for using Global Bank Services")
-            speak("Have a nice day, Bye...")
+
+def Change_Pin():
+    x = int(getpass(speak(" Enter your old pin: ")))
+    if x == atm_pin:
+        update(atm_pin, new_pin)
+        speak("Your pin is successfully changed...., Thankyou for using Global Bank Services")
+        speak("Have a nice day, Bye...")
             
-        else:
-            speak(" Enter a valid pin to proceed ")
-            
+    else:
+        speak(" Enter a valid pin to proceed ")
             
             
-    elif trans == "Transfer Money" :
+def Transfer_Money():
+    x = int(getpass(speak("please re-enter your pin to proceed:")))
+    if x==atm_pin:
         amount = int(input(speak("Enter a amount to transfer:")))
+        another_acc = input(speak("Enter Beneficiary account Holder Name: "))
         if (amount>0):
-            speak("Your amount have successfully transfered, Thankyou for using Global Bank Services")
-            speak("Have a Good day, Bye...")
+            speak("Your amount have successfully transfered to " + another_acc + ", Thankyou for using Global Bank Services")
+            speak("Have a Nice day, Bye...")
             
         else:
             speak("Enter valid amount")
+    
             
+def Quit():
+    quit_1=input(speak("press 'yes' to quit"))
+    if quit_1 == "yes":
+        speak("quit")
+        speak("Collect your card")
+        speak("Thankyou for using Global Bank Services ")
+        speak("Have a Good day, Bye...")
             
-    elif trans == "Quit":
-        quit_1=input(speak("press 'yes' to quit"))
-        if quit_1 == "yes":
-            speak("quit")
-            speak("Collect your card")
-            speak("Thankyou for using Global Bank Services ")
-            speak("Have a Good day, Bye...")
-            
-        else:
-            speak(" Choose any Transaction: ")
-            
-            
+    else:
+        speak(" Choose any Transaction: ") 
+        
+
+def update(atm_pin, new_pin): 
+    
+    new_pin = int(getpass(speak("Enter new pin: ")))
+    atm_pin, new_pin = new_pin, atm_pin
+    return atm_pin
+
+
+        
+atm_pin = 9999
+
+transation = ["Balance Enquiry", "Withdraw Money","Deposit Money", "Change your pin", "Transfer Money", "Quit"]
+
+pin = int(getpass(speak("Enter your 4 digit pin: ")))
+
+if pin==atm_pin:
+    
+    speak(" Choose your Transaction: ")
+    
+    print("1. Balance Enquiry")
+    print("2. Withdraw Money")
+    print("3. Deposit Money")
+    print("4. Change your pin")
+    print("5. Transfer Money")
+    print("6. Quit")
+    trans=int(input("transation: "))
+    time.sleep(2)
+    if trans==1:
+        return Balance_Enquiry()
+    elif trans==2:
+        return Withdraw_Money()
+    elif trans==3:
+        return Deposite_Money()
+    elif trans==4:
+        return Change_Pin()
+    elif trans==5:
+        return Transfer_Money()
+    elif trans==6:
+        return Quit()
+    else:
+        speak("please enter a valid option")
+   
             
 else:
     speak("Wrong pin entered.... Try Again")
